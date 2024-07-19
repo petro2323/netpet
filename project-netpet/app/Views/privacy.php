9a8dc332cbb49b97e4de1c77b70c69620021255c
petro2323 <aleksandarpetronijevic65@gmail.com>
2024-07-19 11:32:56 +0200

    privacy accordion view

--- project-netpet/app/Views/privacy.php
+++ project-netpet/app/Views/privacy.php
@@ -6,34 +6,65 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
     <link rel="icon" href="<?= base_url() ?>/photos/icon.png">
-    <title>NetPet - View Network Details</title>
+    <title>NetPet - Privacy Policy</title>
 </head>
 <body>
 
 <div class="image-container text-center">
     <a href="<?= base_url('/') ?>"><img src="<?= base_url('photos/logo.png') ?>"></a>
 </div>
-    
-<div class="container text-white text-center">
-    <h1><u>Privacy Policy</u></h1>
-</div>
 
-<div class="container text-white text-center mt-4">
-    <p class="fs-5 fw-bold">What type of information does NetPet collect?</p>
-    <p class="fs-5 mt-4">Our services are completely anonymous, and no information is logged.</p>
-    <p class="fs-5 fw-bold mt-4">I see my public IP address is displayed, is this somehow logged or stored?</p>
-    <p class="fs-5 mt-4">The IP address is displayed directly on the screen for your reference only and is not logged or stored in any way.</p>
-    <p class="fs-5 fw-bold mt-4">Does NetPet use cookies?</p>
-    <p class="fs-5 mt-4">No.</p>
-    <p class="fs-5 fw-bold mt-4">How can I be sure that my activity on NetPet remains private?</p>
-    <p class="fs-5">NetPet is committed to ensuring your privacy. We do not log, store, or share any information about your activity. All interactions are anonymous, and any data displayed is for your reference only and is not saved anywhere.</p>
+<div class="container mt-2 mb-1">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button collapsed button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <strong>Does NetPet use cookies?</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            No.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <strong>I see my public IP address is displayed, is this somehow logged or stored?</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            The IP address is displayed directly on the screen for your reference only and is not logged or stored in any way.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <strong>How can I be sure that my activity on NetPet remains private?</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            NetPet is committed to ensuring your privacy. We do not log, store, or share any information about your activity. All interactions are anonymous, and any data displayed is for your reference only and is not saved anywhere.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <footer class="mt-5 text-white py-4">
     <div class="container text-center">
-        <p>&copy; 2024 NetPet v1.0 - <a href="<?= base_url('privacy') ?>" class="text-white">Privacy Policy</a></p>
+        <p>&copy; 2024 NetPet v1.0 - <a href="<?= base_url('/') ?>" class="text-white">Home Page</a></p>
     </div>
 </footer>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
